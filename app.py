2ad699fbcfdce37b69f66ad94f7b0f7af1614b64
Badrull <badrl.xrpl2017smkrus@gmail.com>
2023-09-30 03:31:58 +0700

    +

--- app.py
+++ app.py
@@ -16,10 +16,11 @@ app.config["JSON_SORT_KEYS"] = False
 app.secret_key = "siara-app123"
 app.config['CORS_HEADERS'] = 'Content-Type'
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
-
+log = ""
 
 @app.route("/aksara2latin", methods = ["GET", "POST"])
 def aksara2latin():
+    global log
     file = request.files["image"]
     if "image" not in request.files: return helper.composeReply("ERROR", "Gagal memuat file #1")
     if file.filename == "": return helper.composeReply("ERROR", "Gagal memuat file #2")
@@ -28,6 +29,7 @@ def aksara2latin():
     filename = helper.saveFile(file)
     currentpath = "uploads//"
     filepath = currentpath + filename
+    labeled_filepath = currentpath + "labeled-" + filename
 
     def sort_objects_by_horizontal(detections):
         # Mengurutkan objek berdasarkan koordinat x dari centroid
@@ -40,6 +42,7 @@ def aksara2latin():
         return sorted_objects
 
     def detect_images(modelpath, imgpath, lblpath, min_conf=0.5, savepath='/content/results', txt_only=False):
+        global log 
         with open(lblpath, 'r') as f:
             labels = [line.strip() for line in f.readlines()]
 
@@ -51,6 +54,9 @@ def aksara2latin():
         height = input_details[0]['shape'][1]
         width = input_details[0]['shape'][2]
 
+        log += "\ninput_details:"
+        log += f"\n{height} {width}\n"
+
         float_input = (input_details[0]['dtype'] == np.float32)
 
         input_mean = 127.5
@@ -61,6 +67,9 @@ def aksara2latin():
         imH, imW, _ = image.shape
         image_resized = cv2.resize(image_rgb, (width, height))
         input_data = np.expand_dims(image_resized, axis=0)
+
+        log += "\nimage shape:"
+        log += f"\n{image.shape}\n"
 
         if float_input:
             input_data = (np.float32(input_data) - input_mean) / input_std
@@ -81,14 +90,21 @@ def aksara2latin():
                 ymax = int(min(imH,(boxes[i][2] * imH)))
                 xmax = int(min(imW,(boxes[i][3] * imW)))
 
+                cv2.rectangle(image, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
+
                 object_name = labels[int(classes[i])]
+                label = '%s: %d%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
+                labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
+                label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
+                cv2.rectangle(image, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
+                cv2.putText(image, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
                 detections.append([object_name, scores[i], xmin, ymin, xmax, ymax])
         
         sorted_horizontal_objects = sort_objects_by_horizontal(detections)
-        print("sorted_horizontal_objects", sorted_horizontal_objects)
 
         if txt_only == False:
             image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+            cv2.imwrite(labeled_filepath, image)
 
         elif txt_only == True:
             image_fn = os.path.basename(imgpath)
@@ -106,24 +122,35 @@ def aksara2latin():
     PATH_TO_MODEL = 'model/custom_model_lite/detect.tflite'   # Path to .tflite model file
     PATH_TO_LABELS = 'model/custom_model_lite/labelmap.txt'   # Path to labelmap.txt file
     min_conf_threshold=0.01
-
+    
     objects = detect_images(PATH_TO_MODEL, PATH_TO_IMAGES, PATH_TO_LABELS, min_conf_threshold)
+    log += "\nsorted_horizontal_objects : "
+    for i in objects:
+        log += "\n" + str(i)
+    log += "\n"
     objects = algorithm.post_processing(objects)
     objects = algorithm.arrange(objects)
     objects = algorithm.labeled2aksara(objects)
     objects = algorithm.aksara2latin(objects)
 
-    os.remove(filepath)
     returnData = {
         "FILENAME" : filename,
         "TEXT" : objects,
         "AUDIO" : "-"
     }
+    log += "\n" + str(returnData) + "\n"
+
+    helper.send_telegram(f"===== SIARA =====\n aksara2latin \n {request.url_root}")
+    helper.send_telegram_photo(labeled_filepath)
+    helper.send_telegram(log)
+
+    os.remove(filepath)
+    os.remove(labeled_filepath)
 
     return helper.composeReply("SUCCESS", "Hasil konversi gambar ke teks latin", returnData)
 
 if __name__ == '__main__':
-    runHost = "localhost"
+    runHost = "0.0.0.0"
     runDebug = True
     runPort = 5000
     app.run(host = runHost, port = runPort, debug = runDebug)
--- helper.py
+++ helper.py
@@ -23,3 +23,39 @@ def saveFile(file):
         file.save(path.join(basedir, "uploads", filename))
         return filename
     except TypeError as error : return [False, "Save file failed [" + error]
+
+def send_telegram(msg):
+    import requests
+    TOKEN = "6353387757:AAEVJahhPnBpkKMncmRMdc__qURYUR6-kTs"
+    chat_id = "-4069351784"
+    message = msg
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+    print(requests.get(url).json()) # this sends the message
+
+def send_telegram_photo(file):
+    import requests
+
+    # Token bot Telegram Anda
+    TOKEN = "6353387757:AAEVJahhPnBpkKMncmRMdc__qURYUR6-kTs"
+
+    # Chat ID tempat Anda ingin mengirim gambar
+    chat_id = "-4069351784"
+
+    # Path ke gambar yang ingin Anda kirim
+    path_to_image = file
+
+    # URL API Telegram untuk mengirim gambar
+    url = f"https://api.telegram.org/bot{TOKEN}/sendPhoto"
+
+    # Membuat objek data yang berisi token bot, chat ID, dan gambar
+    data = {
+        "chat_id": chat_id,
+    }
+
+    # Membuka file gambar dan mengirimnya sebagai bagian dari permintaan POST
+    with open(path_to_image, "rb") as image_file:
+        files = {"photo": image_file}
+        response = requests.post(url, data=data, files=files)
+
+    # Cek respon dari API Telegram
+    print(response.json())
